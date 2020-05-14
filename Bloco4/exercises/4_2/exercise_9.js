f00980d726acd9a578cf18b6f2f864a93cd6b77a
hleoc <informig@gmail.com>
2020-05-13 21:18:51 -0300

    Bloco 4

---
+++ Bloco4/exercises/4_2/exercise_9.js
@@ -0,0 +1,12 @@
+//Ordene o array numbers em ordem decrescente e imprima seus valores;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27], posicao;
+
+for (let x = 1; x < numbers.length; x++) {
+    for () {
+        if () {
+
+
+        }
+    }
+}
